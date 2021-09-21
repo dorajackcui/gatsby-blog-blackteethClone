cc71cbbe018b55ce0c906832b9e00cffd7943364
dorajackcui <dorajackcui@gmail.com>
2021-09-21 21:03:53 +0200

    add scroll-up button

--- src/templates/article_details.js
+++ src/templates/article_details.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ArticleLayout from '../components/ArticleLayout'
 import * as styles from '../styles/details.module.css'
 import { graphql } from 'gatsby'
-
+import BackToTop from "react-back-to-top-button";
 
 export default function ArticleDetails ({ data }){
   
@@ -15,6 +15,14 @@ export default function ArticleDetails ({ data }){
       <div className={styles.details}>
         <div className={styles.info}>文：{stack} &nbsp; &nbsp; &nbsp; &nbsp;译：抑之</div>
         <div className={styles.content} dangerouslySetInnerHTML={{ __html: html }} />
+        <BackToTop
+          showOnScrollUp
+          showAt={1000}
+          speed={1500}
+          easing="easeInOutQuint"
+          >
+          <div className={styles.scrollUp}>↑</div>
+        </BackToTop>
       </div>
       
     </ArticleLayout>
