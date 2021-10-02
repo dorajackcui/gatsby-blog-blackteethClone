34534473cfe16d7bc1321ffb19074d64becdad5b
dorajackcui <dorajackcui@gmail.com>
2021-10-03 00:43:23 +0200

    UI change

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,31 +1,25 @@
 import * as React from "react"
-import { Link } from "gatsby"
+import Layout from '../components/Layout'
+
 
 // styles
-const pageStyles = { 
-  padding: "96px",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
+
 
 const paragraphStyles = {
-  marginBottom: 48,
+  marginTop:'3rem',
+  display:'flex',
+  flexDirection: 'column', 
 }
 
 // markup
 const NotFoundPage = () => {
   return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Vertige de la liste</h1>
+    <Layout>
+      <h1>Vertige de la liste</h1>
       <div style={paragraphStyles}>
         <a href="https://www.notion.so/e6fa57bb62204951bfc113371c989114" target="_blank" rel="noreferrer">一些游戏网站</a>
-        <Link to="/">go home</Link>
       </div>
-    </main>
+    </Layout>
   )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -12,10 +12,13 @@ const customStyles = {
   },
   content: {
     position: 'absolute',
-    margin:" 10% 25% 10% 25%",
+    inset:'0px',
+    margin:" 10% auto",
     borderRadius:"0",
     border:'none',
     backgroundColor: 'rgba(255, 253, 250, 0.95)',
+    width:"90%",
+    maxWidth:"750px",
   },
 };
 
@@ -37,7 +40,7 @@ export default function Index({data}) {
   function closeModal() {
     setIsOpen(false);
   }
-
+  Modal.setAppElement('body');
   return (
     <Layout>
       <center style={{marginTop:'25vh'}}>
@@ -62,7 +65,11 @@ export default function Index({data}) {
         style={customStyles}
         contentLabel="Modal"
       >
-        
+        <button onClick={closeModal} className="backHome">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
+            <path d="M14.5 1.5a.5.5 0 0 1 .5.5v4.8a2.5 2.5 0 0 1-2.5 2.5H2.707l3.347 3.346a.5.5 0 0 1-.708.708l-4.2-4.2a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 8.3H12.5A1.5 1.5 0 0 0 14 6.8V2a.5.5 0 0 1 .5-.5z"/>
+          </svg>
+        </button>
         <div className={styles.modalContent} dangerouslySetInnerHTML={{ __html: html }} />
         
       </Modal>
