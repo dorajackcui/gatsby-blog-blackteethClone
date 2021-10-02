94fc1aaaa92f4828cd48a5bd28392c572829dfcd
dorajackcui <dorajackcui@gmail.com>
2021-10-02 22:05:10 +0200

    add feeds to index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,81 @@
 import { StaticImage } from 'gatsby-plugin-image'
-import React from 'react'
+import React, {useState} from 'react'
 import Layout from '../components/Layout'
+import Modal from 'react-modal'
+import { graphql } from 'gatsby'
+import * as styles from '../styles/details.module.css'
 
-const  iconStyle = {
-  marginTop:'25vh',
-  
-}
 
-export default function index() {
+const customStyles = {
+  overlay: {
+    backgroundColor: 'none'
+  },
+  content: {
+    position: 'absolute',
+    margin:" 10% 25% 10% 25%",
+    borderRadius:"0",
+    border:'none',
+    backgroundColor: 'rgba(255, 253, 250, 0.95)',
+  },
+};
+
+
+export default function Index({data}) {
+
+  const {html} = data.markdownRemark
+
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
   return (
     <Layout>
-      <center style={iconStyle}>
-        <StaticImage alt="magic" src="../../static/magic.png"
-          placeholder= 'none'
-          loading='lazy'
-          layout="fixed"
-          imgStyle={{borderRadius:'50%'}}
-          width={160}
-          height={160}
-          />
+      <center style={{marginTop:'25vh'}}>
+        <button onClick={openModal} className={styles.modalButton} >
+          <StaticImage 
+            alt="magic" 
+            src="../../static/magic.png"
+            placeholder= 'none'
+            loading='lazy'
+            layout="fixed"
+            imgStyle={{borderRadius:'50%'}}
+            width={160}
+            height={160}
+            />
+        </button>
       </center>
+
+      <Modal 
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Modal"
+      >
+        
+        <div className={styles.modalContent} dangerouslySetInnerHTML={{ __html: html }} />
+        
+      </Modal>
     </Layout>
   )
 }
+
+
+export const modalQuery = graphql`
+  query modal {
+    markdownRemark(frontmatter: {stack: {eq: "modal"}}) {
+      html
+      
+    }
+  }
+`
