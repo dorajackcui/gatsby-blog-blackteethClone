8d0e600cff466e76912eb1505a728c1568b51ca7
dorajackcui <dorajackcui@gmail.com>
2021-11-24 21:30:00 +0100

    useless update

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,16 +1,11 @@
 import React from 'react'
 import Layout from '../../components/Layout'
-import ModalButton from '../../components/ModalButton'
-
 
 function index() {
-   const handleClick = ()=>{
-     console.log("111")
-   }
+   
   return (
     <Layout>
-      <ModalButton/>
-      <button onClick={handleClick} />
+      PROjects
     </Layout>
   )
 }
